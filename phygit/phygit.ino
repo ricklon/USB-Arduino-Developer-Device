e1f5597501178dd20476b22b13cfbd1507c162fb
Ricklon <rick.rickanderson@gmail.com>
2012-01-02 15:50:41 -0500

    Commands added now time to test.

--- phygit/phygit.ino
+++ phygit/phygit.ino
@@ -13,17 +13,18 @@ Git Controls using custom buttons as Keyboard output
  
  */
 
-#define gitrepo = https://github.com/ricklon/USB-Arduino-Developer-Device
-#define NUMBUTTONS 9
+#define GITREPO  https://github.com/ricklon/USB-Arduino-Developer-Device
+#define ABSDIRECTORY /Users/rianders/projects/USB-Arduino-Developer-Device
+#define NUMBUTTONS 10
 
 long previousMillis = 0;        // will store last time millis was updated
 long interval = 15;           // interval at which to debounce (milliseconds)
 
 const int buttonPins[] = { 
-  26,27,28,29,30,31,32,33,34};
+  26,27,28,29,30,31,32,33,34,35};
 
 int previousButtonState[NUMBUTTONS] ={
-  0,0,0,0,0,0,0,0,0};
+  0,0,0,0,0,0,0,0,0,0};
 
 void setup() 
 {
@@ -58,28 +59,42 @@ void doAction(int button)
   switch(button)
   {
   case 0: //Clone repository
+    Keyboard.print("git clone ");
+    Keyboard.print("GITREPO");
     break;
   case 1: //Fetch changes
+    Keyboard.println("git fetch");
     break;
   case 2: //Check status
+    Keyboard.println("git diff --stat origin/master");
     break;
-  case 3: //check differences
+  case 3: //check differences summary
+    Keyboard.println("git diff --name-only origin/master");
     break;
   case 4: //Get the changes
+    Keyboard.println("");
     break;
   case 5:  //List branches
+    Keyboard.println("git branches");
     break;
   case 6: //Check differences
+    Keyboard.println("git diff");
     break;
   case 7: //Commit changes
+    Keyboard.print("git commit -a -m \"mychange\"");
     break;
   case 8: //Push changes
+    Keyboard.println("git push");
     break;
-
+  case 9: //Change to projcet directory
+    Keyboard.print("cd");
+    Keyboard.println("ABSDIRECTORY");
+    break;
 
 
   }
 }
+
 
 
 
